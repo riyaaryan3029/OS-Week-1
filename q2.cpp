2a1a3492d16419720077d2769df7f9c8a0299609
riyaaryan3029 <23103029@mail.jiit.ac.in>
2025-07-29 14:38:08 +0530

    Question 2 uploaded

---
+++ q2.cpp
@@ -0,0 +1,13 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cout<< "Enter a string : ";
+    cin>>s;
+
+    reverse(s.begin(),s.end());
+    cout<<endl<<s<<endl;
+    return 0;
+}
